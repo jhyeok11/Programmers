67cb8d05957da460828a577169a29b2d44c85454
jhyeok11 <ljh1349@yu.ac.kr>
2026-02-12 19:00:41 +0900

    260113 짝수와 홀수

---
+++ Lv1/짝수와 홀수.py
@@ -0,0 +1,12 @@
+def solution(num):
+    answer = ''
+    
+    if num%2 == 0:
+        answer = "Even"
+    else:
+        answer = "Odd"
+        
+    return answer
+
+print(solution(3))
+print(solution(4))
