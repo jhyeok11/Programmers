ac8116a96c68fa55bf3534c4d5ab58fbfe0f1753
jhyeok11 <ljh1349@yu.ac.kr>
2026-02-13 16:28:54 +0900

    260116 x만큼 간격이 있는 n개의 숫자 문제풀이

---
+++ Lv1/12954/x만큼 간격이 있는 n개의 숫자.py
@@ -0,0 +1,11 @@
+def solution(x, n):
+    answer = []
+    
+    for i in range(n):
+        answer.append(x * (i+1))
+        
+    return answer
+
+print(solution(2, 5))
+print(solution(4, 3))
+print(solution(-4, 2))
