9e56a40483bbfb04dbc25631aa8d1f80fd996db3
jhyeok11 <ljh1349@yu.ac.kr>
2026-02-17 19:40:53 +0900

    260204 제일 작은 수 제거하기 문제풀이

---
+++ Lv1/12935/제일 작은 수 제거하기.py
@@ -0,0 +1,13 @@
+def solution(arr):
+    answer = []
+    
+    if len(arr) == 1:
+        answer = [-1]
+    else:
+        arr.remove(min(arr))
+        answer = arr
+
+    return answer
+
+print(solution([4, 3, 2, 1]))
+print(solution([10]))
