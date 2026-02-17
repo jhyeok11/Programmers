775e5f71483f0d32f351adb9ac34b74ebafd5fbb
jhyeok11 <ljh1349@yu.ac.kr>
2026-02-17 22:28:28 +0900

    260210 가장 가까운 같은 글자 문제풀이

---
+++ Lv1/142086/가장 가까운 같은 글자.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = []
+    
+    answer.append(-1)
+    for i in range(1, len(s)):
+        for j in range(i-1, -1, -1):
+            if s[i] == s[j]:
+                answer.append(i-j)
+                break
+        else:
+            answer.append(-1)
+                
+    return answer
+
+print(solution("banana"))
+print(solution("foobar"))
